45da993033d1842cec81d5018b0552637f93b401
Daniel Stepp <willstepp@gmail.com>
2018-07-27 11:29:07 -0400

    changed icon to yellow

--- web/src/app/components/welcome/welcome.component.ts
+++ web/src/app/components/welcome/welcome.component.ts
@@ -9,14 +9,26 @@ import { StoreActions as Actions } from '../../services/store/store.actions';
 })
 export class WelcomeComponent implements OnInit, OnDestroy {
 
+  darkMode: boolean;
+  dmel: any;
+
   constructor (private storeService: StoreService) { }
 
   ngOnInit() {
+    this.darkMode = this.storeService.local.get('darkMode') === 'true';
     this.storeService.dispatch(Actions.UI.UpdateShowHomeIcon, false);
+    let dmcb = document.getElementById('switch-sm');
+    if (dmcb) this.dmel = dmcb.addEventListener('change', this.updateDarkMode.bind(this));
   }
 
   ngOnDestroy() {
     this.storeService.dispatch(Actions.UI.UpdateShowHomeIcon, true);
+    let dmcb = document.getElementById('switch-sm');
+    if (dmcb) this.dmel = dmcb.removeEventListener('change', this.updateDarkMode.bind(this));
+  }
+
+  updateDarkMode (evt) {
+    this.darkMode = evt.target.checked;
   }
 
 }
