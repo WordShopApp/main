4758d316ab7db82062d4c3a5104f1f9fd86b60d2
Daniel Stepp <willstepp@gmail.com>
2018-08-08 12:20:21 -0400

    updated

--- web/src/app/components/user-settings/user-settings.component.ts
+++ web/src/app/components/user-settings/user-settings.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { Subscription } from 'rxjs';
+
+import { AccountService } from '../../services/account/account.service';
+import { AlertTypes } from '../alert/alert.component';
+import { LoggerService } from '../../services/logger/logger.service';
+import { MessengerService } from '../../services/messenger/messenger.service';
+import { NavService } from '../../services/nav/nav.service';
+import { StoreService } from '../../services/store/store.service';
+import { StoreProps as Props } from '../../services/store/store.props';
+import { StoreActions as Actions } from '../../services/store/store.actions';
+import { WordIconService } from '../../services/word-icon/word-icon.service';
 
 @Component({
   selector: 'app-user-settings',
@@ -12,16 +23,71 @@ export class UserSettingsComponent implements OnInit, OnDestroy {
   area: string;
   param$: Subscription;
 
-  constructor (private route: ActivatedRoute) { }
+  profile: any;
+  profile$: Subscription;
+
+  profileSaveEnabled: boolean;
+  profileNewUsername: string;
+
+  avatarPalette: any;
+
+  constructor (
+    private location: Location,
+    private route: ActivatedRoute,
+    private accountService: AccountService,
+    private loggerService: LoggerService,
+    private messengerService: MessengerService,
+    private navService: NavService,
+    private storeService: StoreService,
+    private wordIconService: WordIconService
+  ) { }
 
   ngOnInit() {
     this.param$ = this.route.paramMap.subscribe(params => {
       this.area = params.get('area');
     });
+    this.profile$ = this.storeService.subscribe(Props.App.Profile, p => {
+      this.profile = p && p.toJS();
+      this.updateAvatarPalette(this.profile.username);
+    });
   }
 
   ngOnDestroy () {
     this.param$.unsubscribe();
+    this.profile$.unsubscribe();
+  }
+
+  home () {
+    this.navService.gotoRoot();
+  }
+
+  saveProfileChanges () {
+    if (this.profileSaveEnabled) {
+      this.accountService.updateProfile({ username: this.profileNewUsername }).then(updated => {
+        this.storeService.dispatch(Actions.Init.Profile, updated);
+        this.home();
+      }).catch(err => {
+        this.loggerService.error(err);
+        this.sendMessage(AlertTypes.Danger, 'Error:', err.message);
+      });
+    }
+  }
+
+  validationResults (res) {
+    this.profileSaveEnabled = res.valid;
+    this.profileNewUsername = res.username;
+  }
+
+  usernameChanged (name) {
+    this.updateAvatarPalette(name);
+  }
+
+  updateAvatarPalette (hash) {
+    this.avatarPalette = this.wordIconService.getPalette(hash);
+  }
+
+  sendMessage(type, header, message) {
+    this.messengerService.send('global:alert', { type, header, message });
   }
 
 }
